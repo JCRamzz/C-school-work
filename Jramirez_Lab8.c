3b058ad22301d512192188dc35544d16b6880a71
Juan Carlos <JCramz1992@gmail.com>
2019-03-31 12:54:31 -0500

    Cleaned up code, added ASCII art splash, everything works good

--- Jramirez_Lab8.c
+++ Jramirez_Lab8.c
@@ -1,18 +1,36 @@
+/*
+Program Name: Roll The Dice!
+Author: Juan Carlos Ramirez
+Description: This simple program will let the user roll a dice, there are 7 choices
+the outcome will always be randomized through the use of various methods and functions.
+Warning: This program will not output an accurate representation of your actual luck.
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <ctype.h>
 
 void dice();
-int randomizer(int num);
+void randomizer(int maxSize);
 void displayMenu();
 void displayTitle();
+void suspendTime();
 
 int main()
 {
-    //puts("Hello World!");
-    dice();
+	puts("═██╗     ██╗   ██╗ ██████╗██╗  ██╗██╗   ██╗    ██████╗ ██╗ ██████╗███████╗");
+	puts(" ██║     ██║   ██║██╔════╝██║ ██╔╝╚██╗ ██╔╝    ██╔══██╗██║██╔════╝██╔════╝");
+	puts(" ██║     ██║   ██║██║     █████╔╝  ╚████╔╝     ██║  ██║██║██║     █████╗  ");
+	puts(" ██║     ██║   ██║██║     ██╔═██╗   ╚██╔╝      ██║  ██║██║██║     ██╔══╝  ");
+	puts(" ███████╗╚██████╔╝╚██████╗██║  ██╗   ██║       ██████╔╝██║╚██████╗███████╗");
+	puts(" ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝       ╚═════╝ ╚═╝ ╚═════╝ ╚═════╝");
+	suspendTime();
+	suspendTime();
+	puts("-------------------------------LOADING------------------------------------");
+	suspendTime();
+	suspendTime(); 
 
-
+	dice();
 
     return 0;
 }
@@ -20,18 +38,18 @@ int main()
 void displayMenu()
 {
 
-    puts(" | ~~~  Choose your weapon   ~~~ |");
-	puts(" |                               |");
-	puts(" |  1) A four-sided die          |");
-	puts(" |  2) A six-sided die           |");
-	puts(" |  3) A eight-sided die         |");
-	puts(" |  4) A ten-sided die           |");
-	puts(" |  5) A Twelve-sided die        |");
-	puts(" |  6) A Twenty-sided die        |");
-    puts(" |  7) A One-Hundred-sided die   |");
-    puts(" |                               |");
-    puts(" |  8) Get me out!!!             |");
-	puts(" |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+    puts("                       | ~~~  Choose your weapon   ~~~ |");
+	puts("                       |                               |");
+	puts("                       |  1) A four-sided die          |");
+	puts("                       |  2) A six-sided die           |");
+	puts("                       |  3) A eight-sided die         |");
+	puts("                       |  4) A ten-sided die           |");
+	puts("                       |  5) A Twelve-sided die        |");
+	puts("                       |  6) A Twenty-sided die        |");
+    puts("                       |  7) A One-Hundred-sided die   |");
+    puts("                       |                               |");
+    puts("                       |  8) Get me out!!!             |");
+	puts("                       |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
 }
 
 void dice()
@@ -39,63 +57,43 @@ void dice()
 
 int choice = 0;
 int maxSides = 0;
+int index = 0;
 
 do
 	{
 	    displayMenu();
-        printf("Enter your choice:");	
+        printf("\n                                 >> ");	
 	    scanf("%d", &choice);
 		
 		switch (choice)
 		{
             case 1: 
-	            //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 4) + 1;
-		        // Between 1 and 4
 				maxSides = 4;
-	            printf("Random number: %d", randomizer(maxSides));
+	    		randomizer(maxSides);
                 break;
             case 2:
-                //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 6) + 1;
-		        // Between 1 and 6
 				maxSides = 6;
-	            printf("Random number: %d", randomizer(maxSides));
+	            randomizer(maxSides);
 				break;
             case 3:
-                //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 8) + 1;
-		        // Between 1 and 8
 				maxSides = 8;
-	            printf("Random number: %d", randomizer(maxSides));
+	            randomizer(maxSides);
                 break;
             case 4:
-                //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 10) + 1;
-		        // Between 1 and 10
 				maxSides = 10;
-	            printf("Random number: %d", randomizer(maxSides));
+	            randomizer(maxSides);
             	break;
             case 5:
-                //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 12) + 1;
-		        // Between 1 and 12
 				maxSides = 12;
-	            printf("Random number: %d", randomizer(maxSides));
+	            randomizer(maxSides);
                 break;
             case 6:
-                //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 20) + 1;
-		        // Between 1 and 20
 				maxSides = 20;
-	            printf("Random number: %d", randomizer(maxSides));
+	            randomizer(maxSides);
             	break;
             case 7:
-                //srand((unsigned int)time(NULL));
-	            //randNum = (rand() % 100) + 1;
-		        // Between 1 and 100
 				maxSides = 100;
-	            printf("Random number: %d", randomizer(maxSides));
+	            randomizer(maxSides);
             	break;
             case 8:
                 puts("Thank you, bye!");
@@ -104,24 +102,47 @@ do
                 printf("Error!\n");
                 break;
 		}
-	}while (choice != 8);
+	}while (choice != 8); //validation
    
 }
 
-int randomizer(int maxSides)
+void randomizer(int maxSides) // All the randomizing will be done in this function, output as well.
 {
 	int sides = maxSides;
 	int randNum = 0;
 	srand((unsigned int)time(NULL));
 	randNum = (rand() % sides) + 1;
-	// Between 1 and localNum
+	// Between 1 and sides
 	if (randNum == 1)
 	{
-		puts("Yikes, you got a 1, the lowest possible outcome, how unlucky!");
+		puts("                        Ready?! Here we go!!!");
+		puts("                        Rolling...");
+		suspendTime(); //Pause for dramatic effect of course
+		puts("        Yikes, you got a 1, the lowest possible outcome, how unlucky!"); //lowest possible outconme
 	}
 	else if (randNum == sides)
 	{
-		puts("Wow, you got %d the highest possible outcome, go buy a lottery ticket!");
+		puts("                        Ready?! Here we go!!!");
+		puts("                        Rolling...");
+		suspendTime();
+		printf("    Wow, you got %d the highest possible outcome, go buy a lottery ticket!\n", randNum); //highest outcome
 	}
-	return randNum;
+	else
+	{
+		puts("                        Ready?! Here we go!!!");
+		puts("                        Rolling...");
+		suspendTime();
+		printf("                      You've rolled a %d!\n", randNum);
+
+	}
+}
+
+void suspendTime() //This function will stop the time for about 2.08 seconds
+{
+	int index = 0;
+	struct timespec ts = {1, 100000000L };
+	for (index = 0; index < 1; index++)
+	{
+		nanosleep(&ts, NULL);
+	}
 }
